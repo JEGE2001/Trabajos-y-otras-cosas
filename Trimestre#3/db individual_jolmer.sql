02ded857c3c5654aa8426f30c14fba7db2a1ee0d
Jolmer <jegiraldo577@misena.edu.co>
2019-09-18 16:59:13 -0500

    Docs: se actualizo y dejo funcional e.e

--- Trimestre#3/db individual_jolmer.sql
+++ Trimestre#3/db individual_jolmer.sql
@@ -75,3 +75,55 @@ create database individual_jolmer;
 	pkfk_id_producto_en INT(7) not null,
 	primary key(pkfk_cod_factura_entrada,pkfk_id_producto_en)
 	);
+    
+
+    /*se genera la foreign key de la tabla persona*/
+	alter table persona add constraint pkfk_tdoc_per
+	foreign key(pkfk_tdoc_per) references tipo_documento(cod_tdoc);
+
+	/*se generan las foreign keys de la tabla cuenta */
+	alter table cuenta add constraint id_cuenta 
+	foreign key(id_cuenta,tdoc_cuenta) references persona(id_persona,pkfk_tdoc_per);
+
+	alter table cuenta add constraint fk_numero_pregunta
+	foreign key(fk_numero_pregunta) references pregunta_de_seguridad(numero_pregunta);
+
+	/*se genera la foreign key de la tabla vendedor*/
+	alter table vendedor add constraint id_vendedor 
+	foreign key(id_vendedor,tdoc_vendedor) references cuenta(id_cuenta,tdoc_cuenta);
+
+	/*se genera la foreign key de la tabla cliente*/
+	alter table cliente add constraint id_cliente 
+	foreign key(id_cliente,tdoc_cliente) references persona(id_persona,pkfk_tdoc_per);
+
+	/*se genera la foreign key de la tabla proveedor*/
+	alter table proveedor add constraint id_proveedor 
+	foreign key(id_proveedor,tdoc_proveedor) references persona(id_persona,pkfk_tdoc_per);
+
+	/*se generan las foreign keys de la tabla factura salida*/
+	alter table factura_salida add constraint fk_id_cliente
+	foreign key(fk_id_cliente,fk_tdoc_cliente) references cliente(id_cliente,tdoc_cliente);
+
+	alter table factura_salida add constraint fk_id_vendedor
+	foreign key(fk_id_vendedor,fk_tdoc_vendedor) references vendedor(id_vendedor,tdoc_vendedor);
+
+	/*se genera la foreign key de la tabla factura entrada*/
+	alter table factura_entrada add constraint fk_id_proveedor
+	foreign key(fk_id_proveedor,fk_tdoc_proveedor) references proveedor(id_proveedor,tdoc_proveedor);
+
+	/*se generan las foreign keys de la tabla factura salida producto*/
+	alter table factura_salida_prducto add constraint pkfk_cod_factura_salida
+	foreign key(pkfk_cod_factura_salida) references factura_salida(cod_factura_salida);
+
+	alter table factura_salida_prducto add constraint pkfk_id_producto_sa 
+	foreign key(pkfk_id_producto_sa) references producto(id_producto);
+
+	/*se generan las foreign keys de la tabla factura entrada producto*/
+	alter table factura_entrada_prducto add constraint pkfk_cod_factura_entrada
+	foreign key(pkfk_cod_factura_entrada) references factura_entrada(cod_factura_entrada);
+
+	alter table factura_entrada_prducto add constraint pkfk_id_producto_en 
+	foreign key(pkfk_id_producto_en) references producto(id_producto);
+
+
+	alter table persona add index idx_pkfk_cod_td(pkfk_cod_td);
